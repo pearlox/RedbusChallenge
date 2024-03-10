a92a0f2a5810cf4599a2e8447f6ce629b7e7b36f
Muthukalai <muthukalai@yahoo.com>
2024-03-10 18:19:25 +0530

    updated scripts to CSS Selectors

--- src/main/java/RedbusChallenge.java
+++ src/main/java/RedbusChallenge.java
@@ -11,14 +11,17 @@ import java.util.Map;
 
 public class RedbusChallenge {
 
-    public static String holidayCounter = "//div[@id='onwardCal']//div[contains(@class,'DayNavigator__CalendarHeader')]//div[@class='holiday_count']";
-    public static String currentMonthName = "//div[@id='onwardCal']//div[contains(@class,'DayNavigator__CalendarHeader')]//div[not(contains(@style,'cursor')) and not(contains(@class,'holiday'))]";
-    public static String weekendDates = "//div[contains(@class,'DayTiles__CalendarDaysBlock')]//span[contains(@class,'DayTiles__CalendarDaysSpan') and contains(@class,'bwoYtA')]";
-    public static String arrowKey = "//div[@id='onwardCal']//div[contains(@class,'DayNavigator__CalendarHeader')]//div[not(contains(@style,'cursor'))]/following::div//*[name()='svg']";
+    //    public static String holidayCounter = "//div[@id='onwardCal']//div[contains(@class,'DayNavigator__CalendarHeader')]//div[@class='holiday_count']";
+    //    public static String currentMonthName = "//div[@id='onwardCal']//div[contains(@class,'DayNavigator__CalendarHeader')]//div[not(contains(@style,'cursor')) and not(contains(@class,'holiday'))]";
+    //    public static String weekendDates = "//div[contains(@class,'DayTiles__CalendarDaysBlock')]//span[contains(@class,'DayTiles__CalendarDaysSpan') and contains(@class,'bwoYtA')]";
+    //    public static String arrowKey = "//div[@id='onwardCal']//div[contains(@class,'DayNavigator__CalendarHeader')]//div[not(contains(@style,'cursor'))]/following::div//*[name()='svg']";
+
+    public static String holidayCounter = ".holiday_count";
+    public static String currentMonthName = "div[class*='DayNavigator__CalendarHeader'] > div:nth-child(2)";
+    public static String weekendDates = "span[class*='bwoYtA']";
+    public static String arrowKey = "div[class*='DayNavigator__CalendarHeader'] > div:nth-child(3) #Layer_1";
 
     public static void main(String[] args) {
-        /*System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")
-                + File.separator+"Webdrivers"+File.separator+"chromedriver.exe");*/
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -41,33 +44,38 @@ public class RedbusChallenge {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String targetMonth = "Dec 2024";
 
-        getWeekendDates(driver, targetMonth);
+        List<String> weekendDateList = getWeekendDates(driver, targetMonth);
+        System.out.println("Weekend List ==> "+weekendDateList);
 
         driver.quit();
     }
 
-    private static void getWeekendDates(WebDriver driver, String targetMonth) {
+    private static List<String> getWeekendDates(WebDriver driver, String targetMonth) {
         driver.get("https://www.redbus.in/");
-        driver.findElement(By.xpath("//div[@id='onwardCal']")).click();
-        WebElement currentMonthElement = driver.findElement(By.xpath(currentMonthName));
+//        driver.findElement(By.xpath("//div[@id='onwardCal']")).click();
+        driver.findElement(By.cssSelector("#onwardCal")).click();
+        WebElement currentMonthElement = driver.findElement(By.cssSelector(currentMonthName));
 
         String currentMonth = currentMonthElement.getText();
         while(!(currentMonth.contains(targetMonth))) {
-            driver.findElement(By.xpath(arrowKey)).click();
+//            driver.findElement(By.xpath(arrowKey)).click();
+            driver.findElement(By.cssSelector(arrowKey)).click();
             currentMonth = currentMonthElement.getText();
         }
         System.out.println("Current Month ==> "+currentMonth.split("\\r?\\n")[0]);
-        List<WebElement> holidayElements = driver.findElements(By.xpath(holidayCounter));
+        List<WebElement> holidayElements = driver.findElements(By.cssSelector(holidayCounter));
         if(!holidayElements.isEmpty()) {
             System.out.println("Holidays ==> "+holidayElements.get(0).getText());
         } else {
             System.out.println("Holidays ==> No holidays");
         }
-        List<WebElement> weekendDateElements = driver.findElements(By.xpath(weekendDates));
+//        List<WebElement> weekendDateElements = driver.findElements(By.xpath(weekendDates));
+        List<WebElement> weekendDateElements = driver.findElements(By.cssSelector(weekendDates));
         List<String> weekendDateList = new ArrayList<>();
         for(WebElement element:weekendDateElements) {
             weekendDateList.add(element.getText().trim());
         }
-        System.out.println(weekendDateList);
+
+        return weekendDateList;
     }
 }
